921acf6e11c8e6a607ff1876df460465c9e305bc
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-02-27 21:23:41 +0100

    minors: prepare simulation

--- src/main/scala/it/unibo/casestudy/SimulationTest.scala
+++ src/main/scala/it/unibo/casestudy/SimulationTest.scala
@@ -14,17 +14,24 @@ object SimulationTest extends App {
   val range = 10
   val delta = 100 milliseconds
   val totalTime = 80 seconds
+  val switchTime = totalTime / 2
   val leftmost = 1
+  val rightmost = 100
+
   def newSimulator(fireLogic: ID => RoundEvent): (ExperimentTrace[Int], ExperimentTrace[Double]) = {
     val world = StandardWorld.withRange(count, count, range, Set(leftmost))
     val des = new DesSimulator(world)
     val fireEvents = des.network.ids.map(fireLogic(_))
-    fireEvents.foreach(des.schedule)
-    des.stopWhen(des.now.plusMillis(totalTime.toMillis))
     val roundCount = Exports.NumericValueExport.fromSensor[Int](des.now, 1 seconds, ExperimentConstant.RoundCount)
     val totalGradient = Exports.NumericValueExport.`export`[Double](des.now, 1 seconds)
+    val turnOffLeft = ChangeSourceAt(des.now.plusMillis(switchTime.toMillis), leftmost, value = false)
+    val turnOnRight = ChangeSourceAt(des.now.plusMillis(switchTime.toMillis), rightmost, value = true)
     des.schedule(roundCount)
     des.schedule(totalGradient)
+    des.schedule(turnOffLeft)
+    des.schedule(turnOnRight)
+    fireEvents.foreach(des.schedule)
+    des.stopWhen(des.now.plusMillis(totalTime.toMillis))
     DesUtils.consume(des)
     (roundCount.trace, totalGradient.trace)
   }
@@ -34,16 +41,30 @@ object SimulationTest extends App {
   val (adjustableFrequency, adjustableGradient) =
     newSimulator(id => AdjustableEvaluation(id, new GradientProgram, Instant.ofEpochMilli(0), delta, 2 seconds, delta))
 
-  val standardPlot = gradientStandard.values.map { case (time, data) => time.toEpochMilli.toDouble -> data.toDouble }
-  val adjustablePlot = adjustableGradient.values.map { case (time, data) =>
-    time.toEpochMilli.toDouble -> data.toDouble
+  val fixedOutputPlot = gradientStandard.values.map { case (time, data) => time.getEpochSecond.toDouble -> data }
+  val adjustableOutputPlot = adjustableGradient.values.map { case (time, data) =>
+    time.getEpochSecond.toDouble -> data
   }
-  val plot = xyplot(
-    (standardPlot.toList, List(line()), InLegend("Periodic")),
-    (adjustablePlot.toList, List(line()), InLegend("Adjustable"))
+
+  val fixedFrequencyPlot = standardFrequency.values.map { case (time, data) =>
+    time.getEpochSecond.toDouble -> data.toDouble
+  }
+  val adjustableFrequencyPlot = adjustableFrequency.values.map { case (time, data) =>
+    time.getEpochSecond.toDouble -> data.toDouble
+  }
+  val outputPlot = xyplot(
+    (fixedOutputPlot.toList, List(line(color = Color.red)), InLegend("Periodic")),
+    (adjustableOutputPlot.toList, List(line(color = Color.blue)), InLegend("Adjustable"))
   )(
-    par(xlab = "x axis label", ylab = "y axis label")
+    par(xlab = "time", ylab = "total output")
   )
 
-  show(plot)
+  val frequencyPlot = xyplot(
+    //(fixedFrequencyPlot.toList, List(line(color = Color.red)), InLegend("Periodic")),
+    (adjustableFrequencyPlot.toList, List(line(color = Color.blue)), InLegend("Adjustable"))
+  )(
+    par(xlab = "time", ylab = "total frequency")
+  )
+
+  show(sequence(List(outputPlot, frequencyPlot), TableLayout(2)))
 }
